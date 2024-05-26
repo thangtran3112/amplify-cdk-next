72506422a43d4dbfb136edef7b187dc2e8565e8c
Thang Tran <trathanl@amazon.com>
2024-05-26 11:22:46 -0700

    Fixed stages and aws config

--- awsconfig.ts
+++ awsconfig.ts
@@ -1,20 +1,20 @@
-import cdkoutput from './_backend/output.json'
+import cdkoutput from "./_backend/output.json";
 
-const output = cdkoutput['fullstack-nextjs-cdk-starter-dev-Stack']
+const output = cdkoutput["appsync-amplify-cdk-next-Stack"];
 
 export const config = {
-	Auth: {
-		Cognito: {
-			userPoolId: process.env.userPoolId || output.UserPoolId,
-			userPoolClientId: process.env.userPoolClientId || output.UserPoolClientId,
-			identityPoolId: process.env.identityPoolId || output.IdentityPoolId,
-		},
-	},
-	API: {
-		GraphQL: {
-			endpoint: process.env.apiUrl || output.GraphQLAPIURL,
-			region: process.env.region || output.Region,
-			defaultAuthMode: 'userPool' as any,
-		},
-	},
-}
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.userPoolId || output.UserPoolId,
+      userPoolClientId: process.env.userPoolClientId || output.UserPoolClientId,
+      identityPoolId: process.env.identityPoolId || output.IdentityPoolId,
+    },
+  },
+  API: {
+    GraphQL: {
+      endpoint: process.env.apiUrl || output.GraphQLAPIURL,
+      region: process.env.region || output.Region,
+      defaultAuthMode: "userPool" as any,
+    },
+  },
+};
